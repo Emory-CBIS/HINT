755b789bdfe94ed540c7800277b162f02ea0db53
JoshLukemire <joshlukemire@gmail.com>
2018-06-16 09:55:05 -0400

    New version of EM that does not store the demeaned Y in memory. Is a bit slower than old version, but does not crash for larger N. Need to do something similar for miu_temp_alt

--- src/UpdateThetaBetaAprx_Vect_Experimental.m
+++ src/UpdateThetaBetaAprx_Vect_Experimental.m
@@ -102,7 +102,12 @@ function [theta_new, beta_new, z_mode, subICmean, subICvar,...
     clear('betaTimesX');
     
     % Observed data with mean subtracted off
-    Y_star_alt = bsxfun(@minus, mtimesx(A', Y),  miu_temp_ALT );
+   % tic()
+    %Y_star_alt = bsxfun(@minus, mtimesx(A', Y),  miu_temp_ALT );
+    %oldtime = toc();
+    %tic()
+    Y_star_uncent = mtimesx(A', Y);
+    %newtime = toc();
 
     % Second and third level variance
     for i = 1:(q + 1)
@@ -125,16 +130,83 @@ function [theta_new, beta_new, z_mode, subICmean, subICvar,...
     subj_sd = zeros(q, 1, q+1);
     for iSubj = 1:N
         % Grab this subjects processed data
-        Y_star_subj = Y_star_alt( q*(iSubj-1)+1:q*iSubj ,:,:);
+        % REMOVE THIS ALLOCATION
+        %Y_star_subj = Y_star_alt( q*(iSubj-1)+1:q*iSubj ,:,:);
         cov_index = iSubj:N:(N*(q+1));
         subj_sd_temp = (mvn_cov_tran_wide(:,:, cov_index ));
         for ii=1:(q+1)
             subj_sd(:,:,ii) = diag(subj_sd_temp(:,:,ii));
         end
         % Calculate the probabilities
-        prob = normpdf(Y_star_subj, 0, subj_sd) + 0.00000000000000000001;
+        prob = normpdf(Y_star_uncent( q*(iSubj-1)+1:q*iSubj ,:),...
+            miu_temp_ALT( q*(iSubj-1)+1:q*iSubj ,:,:), subj_sd) + 0.00000000000000000001;
         probBelong = probBelong + sum( log(prob), 1 );
     end
+    %newtime = newtime + toc();
+%     probBelong2 = probBelong;
+%     
+%         tic()
+        
+    % betaTimesXtemp = mtimesx( X_mtx, 't', beta);
+    % betaTimesX = reshape(permute(betaTimesXtemp, [2 1 3]), [N * q, V]);    
+    % miu_temp_ALT = bsxfun(@plus, BX, betaTimesX);
+% 
+%         
+%     % Calculate the probability of belonging to ICs over subjects
+%     probBelong = bsxfun( @plus ,zeros(1, q+1, V), log(pi_z_prod) );
+%     probBelong = permute(probBelong, [1,3,2]);
+%     % This version of the loop does 20 subjects at a time
+%     nBlock = ceil(N / 20);
+%     nFinalBlock = N - (nBlock-1)*20;
+%     % Sequence of numbers to add to the covariance indices
+%     covSeqAdd = 0:N:(N*(q));
+%     covSeqAdd = kron(ones(20, 1), covSeqAdd);
+%     Nel = 20;
+%     subj_sd = zeros(q*Nel, 1, q+1);
+%     for iBlock = 1:nBlock
+%         % Indices for the subject and mean variables
+%         bStart = (1+(20*q*(iBlock-1)));
+%         bEnd = min( 20*q*iBlock, q*N );
+%         % Indices for the covariance block
+%         startPoint = (1:20) + (iBlock-1) * 20;
+%         % Fix indices if on the last block
+%         if iBlock == nBlock
+%             startPoint = startPoint(startPoint <= N);
+%             Nel = size(startPoint, 2);
+%             covSeqAdd = covSeqAdd(1:Nel, :);
+%             subj_sd = zeros(q*Nel, 1, q+1);
+%         end
+%         startPoint = startPoint' .* ones(size(startPoint, 2), size(covSeqAdd, 2));
+%         cov_index = startPoint + covSeqAdd;
+%         subj_sd_temp = (mvn_cov_tran_wide(:,:, cov_index ));
+%         for ii=1:(q+1)
+%             tempcov = mvn_cov_tran_wide(:,:,cov_index(:,ii));
+%             diagInd = cumsum([1:(q+1):q^2; q^2.*ones(Nel-1,q)]);
+%             subj_sd(:,:,ii) = (reshape( tempcov(diagInd'), [q*Nel,1] ));
+%         end
+%         % Calculate the probabilities
+%         tic()
+%         prob = normpdf(Y_star_uncent( bStart:bEnd ,:),...
+%             miu_temp_ALT(bStart:bEnd ,:,:), subj_sd) + 0.00000000000000000001;
+%         
+%         
+%         toc()
+%         tic()
+%         normpdf(Y_star_uncent( 1:279 ,:),...
+%             miu_temp_ALT(1:279 ,:,:), subj_sd(1:279, :, :)) + 0.00000000000000000001;
+%         toc()
+%         tic()
+%         normpdf(Y_star_uncent( 1:14 ,:),...
+%             miu_temp_ALT(1:14 ,:,:), subj_sd(1:14, :, :)) + 0.00000000000000000001;
+%         toc()
+%         %prob = normpdf(Y_star_uncent( bStart:bEnd ,:),...
+%         %    bsxfun(@plus, BX(bStart:bEnd, :, :), betaTimesX(bStart:bEnd,:)),...
+%         %    subj_sd) + 0.00000000000000000001;
+%         
+%         probBelong = probBelong + sum( log(prob), 1 );
+%     end
+%     NEWESTTIME = toc();
+    
     
     % Remove things no longer needed
     clear('prob')
@@ -156,6 +228,8 @@ function [theta_new, beta_new, z_mode, subICmean, subICvar,...
     w2PrimeSigmaInv = W2' * Sigma1_inv;
     miu_gamma_all = zeros((N + 1) * q, V );
     miu_temp_add = zeros((N + 1) * q, V );
+    
+    % note: Ystar alt = Y_star_uncent - miu_temp_ALT
         
     % Store the needed terms for s0, si, beta only for the mode z
     % configuration
@@ -164,15 +238,28 @@ function [theta_new, beta_new, z_mode, subICmean, subICvar,...
         Sigma_star_all(:,:,ic) =  P * Sigma_gamma_all(:,:,ic) * P';
         miu_gamma_all_ic(:,:,ic) = Sigma_gamma_all(:,:,ic) * w2PrimeSigmaInv;
         yind = find( VoxelIC == ic);
-        miu_gamma_all(:, yind) = (miu_gamma_all_ic(:,:,ic) * Y_star_alt(:, yind,ic));
+        % New Way
+        %tic()
+        miu_gamma_all(:, yind) = miu_gamma_all_ic(:,:,ic) *...
+            (Y_star_uncent(:, yind) - miu_temp_ALT(:, yind, ic));
+        %newtime = newtime + toc();
+        % Old Way
+        %tic()
+        %miu_gamma_all(:, yind) = (miu_gamma_all_ic(:,:,ic) * Y_star_alt(:, yind,ic));
+        %oldtime = oldtime + toc();
         miu_temp_add(1:(q * N), yind) = miu_temp_ALT(:, yind, ic);
         miu_temp_add((q * N)+1 : (N+1) * q, yind) = ...
                             repmat( miu3z(:,:,ic), [1, length(yind)]); 
     end 
-    clear('Y_star_alt')
+    tic()
+    %clear('Y_star_alt')
+    %oldtime = oldtime + toc();
     clear('miu_temp_ALT')
     pMiuGamma = P * miu_gamma_all;
     clear('miu_gamma_all')
+    
+    %disp(oldtime)
+    %disp(newtime)
     
     % overall mean
     miu_star_all = pMiuGamma + miu_temp_add;
