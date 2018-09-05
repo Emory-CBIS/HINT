311f204359c4db93490d7a33690ff363fb26b426
JoshLukemire <joshlukemire@gmail.com>
2018-09-05 18:12:29 -0400

    Added saving jpg

--- displayWindow/displayResults.m
+++ displayWindow/displayResults.m
@@ -74,9 +74,10 @@ end
             'Resize','on',...
             'Visible','off');
         fileMenu = uimenu('Label','File');
-        uimenu(fileMenu,'Label','Save','Callback','disp(''save'')');
-        uimenu(fileMenu,'Label','Quit','Callback','disp(''exit'')',...
-            'Separator','on','Accelerator','Q');
+        %uimenu(fileMenu,'Label','Save','Callback','disp(''save'')');
+        uimenu(fileMenu, 'Label', 'Save to JPG', 'Callback', @save_jpg);
+        %uimenu(fileMenu,'Label','Quit','Callback','disp(''exit'')',...
+        %    'Separator','on','Accelerator','Q');
         viewerMenu = uimenu('Label', 'View', 'Tag', 'viewerMenu');
         uimenu(viewerMenu, 'Label', 'Population', 'Separator', 'On', 'Callback', @stGrp);
         uimenu(viewerMenu, 'Label', 'Sub-Population', 'Callback', @stSubPop);
@@ -1998,5 +1999,36 @@ end
             warnbox = warndlg('No contrasts have been specified')
         end
     end
+
+
+    function save_jpg(hObject, callbackdata)
+        
+        startString = fullfile(ddat.outdir, [ddat.outpre, '_myfigure.jpg']);
+        definput = {startString};
+        answer = inputdlg('Please enter the filename for the image.',...
+            'Input filename',...
+            [1, 70], definput);
+                
+        picfig = figure;
+        p1 = subplot(2, 2, 1);
+        copyobj(ddat.axial_image{1}, p1)
+        p1.YLim = [0, size(ddat.axial_image{1}.CData, 1)];
+        p1.XLim = [0, size(ddat.axial_image{1}.CData, 2)];
+        p1.XTick = []; p1.YTick = [];
+
+        p2 = subplot(2, 2, 2);
+        copyobj(ddat.sagittal_image{1}, p2)
+        p2.XLim = [0, size(ddat.sagittal_image{1}.CData, 2)];
+        p2.XTick = []; p2.YTick = [];
+
+        p3 = subplot(2, 2, 3);
+        copyobj(ddat.coronal_image{1}, p3)
+        p3.XLim = [0, size(ddat.coronal_image{1}.CData, 2)];
+        p3.XTick = []; p3.YTick = [];
+        
+        saveas(picfig, answer{1})
+        delete(picfig)
+    end
+
 
 end
