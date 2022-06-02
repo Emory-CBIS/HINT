8ae9a8fc651799a7094689d5f1299166b2093a17
Joshua Lukemire <joshua.lukemire@emory.edu>
2022-06-02 12:44:49 -0400

    add prewhitening checks for missing data and for brains with only a single value at voxel

--- src/prewhiten_time_courses.m
+++ src/prewhiten_time_courses.m
@@ -1,8 +1,21 @@
 function whitenedData = prewhiten_time_courses(timeCourses, nIC)
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
+%prewhiten_time_courses Function to whiten the data from T time points to
+%nIC time points. Used in initial preprocessing for both hcica and
+%longitudinal hcica
 
+    % Center each voxel's time course
     [X_tilde_all, ] = remmean(timeCourses);
+    
+    % Make sure not all 0
+    uniqueVals = unique(X_tilde_all(:));
+    if numel(uniqueVals) == 1
+        error(['ERROR - all values in brain are: ' num2str(uniqueVals) '. Please check input data.']);
+    end
+    
+    % Check for NANs
+    if any(isnan(X_tilde_all(:)))
+        error(['ERROR - NAs detected in time courses. Please check brain mask.']);
+    end
 
     [U_incr, D_incr] = pcamat(X_tilde_all, 1,...
         size (X_tilde_all, 1),'off', 'off');
